4144a64dddc9ca90b7df961191300004098321ee
Paul Phillips <paulp@improving.org>
2010-08-24 23:19:44 -0700

    Gave the project class a slightly more accurate name.

--- project/build/AwesomeProject.scala
+++ project/build/AwesomeProject.scala
@@ -1,7 +1,8 @@
 import sbt._
 
-class AsyncHttpProject(info: ProjectInfo) extends DefaultProject(info) {
-  //val toolsSnapshot = "scala-tools-snapshot" at "http://scala-tools.org/repo-snapshots"
-  // This is a magical value that I had to figure out by digging through the maven repo.
-  val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
+class AwesomeProject(info: ProjectInfo) extends DefaultProject(info) {
+  val localMaven   = "Maven" at "file://"+Path.userHome+"/.m2/repository"
+  val localIvy     = "Ivy" at "file://"+Path.userHome+"/.ivy2/local"
+  
+  val specs      = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources()
 }
