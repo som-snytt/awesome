0fa978749f82686332cf2e11005cb24cd54432c9
Paul Phillips <paulp@improving.org>
2010-09-09 11:04:45 -0700

    Removed bundled libs in favor of sbt dependencies.

--- project/build/AwesomeProject.scala
+++ project/build/AwesomeProject.scala
@@ -4,5 +4,8 @@ class AwesomeProject(info: ProjectInfo) extends DefaultProject(info) {
   val localMaven   = "Maven" at "file://"+Path.userHome+"/.m2/repository"
   val localIvy     = "Ivy" at "file://"+Path.userHome+"/.ivy2/local"
   
-  val specs      = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources()
+  val scalap    = "org.scala-lang" % "scalap" % "2.8.0" withSources()
+  val paranamer = "com.thoughtworks.paranamer" % "paranamer" % "2.2.1"
+  val asm       = "asm" % "asm-debug-all" % "3.2"
+  val specs     = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources()
 }
