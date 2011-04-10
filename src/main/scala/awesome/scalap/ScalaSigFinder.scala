bcff14cf49e9d49d2d19e01d3765f2e049d3b75a
Paul Phillips <paulp@improving.org>
2011-04-10 14:29:08 -0700

    Removed CopiedScalaSigParser (ScalaSigParser was somehow left out of 2.8.0) bceause it's in 2.8.1.

--- src/main/scala/awesome/scalap/ScalaSigFinder.scala
+++ src/main/scala/awesome/scalap/ScalaSigFinder.scala
@@ -5,7 +5,7 @@ import scala.tools.nsc.util.ScalaClassLoader
 import scala.tools.scalap._
 import Main.{ SCALA_SIG, SCALA_SIG_ANNOTATION, BYTES_VALUE }
 import scalax.rules.{ scalasig => sp }
-import sp.{ ClassFile, ClassFileParser, ScalaSig, ScalaSigAttributeParsers, StringBytesPair }
+import sp.{ ClassFile, ClassFileParser, ScalaSig, ScalaSigParser, ScalaSigAttributeParsers, StringBytesPair }
 import ClassFileParser.{ ConstValueIndex, Annotation }
 import scala.reflect.generic.{ ByteCodecs, PickleBuffer }
 
@@ -22,11 +22,30 @@ object ScalaSigFinder {
   def nameToBytes(name: String): Option[Array[Byte]] =
     findClazz(name) flatMap bytes headOption
   
-  def bytes(clazz: Class[_]): Option[Array[Byte]] =
-    CopiedScalaSigParser bytes clazz
+  def bytes(clazz: Class[_]): Option[Array[Byte]] = {
+    import ScalaSigParser._
+    val byteCode  = sp.ByteCode.forClass(clazz)
+    val classFile = ClassFileParser.parse(byteCode)
+
+    scalaSigBytesFromAnnotation(classFile)
+  }
+  def scalaSigBytesFromAnnotation(classFile: ClassFile): Option[Array[Byte]] = {
+    import ScalaSigParser._
+    import classFile._
+
+    classFile.annotation(SCALA_SIG_ANNOTATION) map {
+      case Annotation(_, elements) =>
+        val bytesElem = elements.find(elem => constant(elem.elementNameIndex) == BYTES_VALUE).get
+        val bytes = ((bytesElem.elementValue match {case ConstValueIndex(index) => constantWrapped(index)})
+                .asInstanceOf[StringBytesPair].bytes)
+        val length = ByteCodecs.decode(bytes)
+      
+        bytes take length
+    }
+  }  
   
   def fromClazz(clazz: Class[_]): Option[ScalaSig] =
-    CopiedScalaSigParser parse clazz
+    ScalaSigParser parse clazz
   
   def fromName(name: String): Option[ScalaSig] = {
     for (loader <- defaultLoaders) {
@@ -42,51 +61,4 @@ object ScalaSigFinder {
   //   defaultLoaders flatMap (_ tryToLoadClass name flatMap fromClazz toList) headOption
   
   def apply(name: String) = fromName(name)
-
-  /** Basically copied from trunk since it missed 2.8.0. */
-  object CopiedScalaSigParser {
-    def bytes(clazz : Class[_]): Option[Array[Byte]] = {
-      val byteCode  = sp.ByteCode.forClass(clazz)
-      val classFile = ClassFileParser.parse(byteCode)
-
-      scalaSigBytesFromAnnotation(classFile)
-    }
-    def scalaSigBytesFromAnnotation(classFile: ClassFile): Option[Array[Byte]] = {
-      import classFile._
-
-      classFile.annotation(SCALA_SIG_ANNOTATION) map {
-        case Annotation(_, elements) =>
-          val bytesElem = elements.find(elem => constant(elem.elementNameIndex) == BYTES_VALUE).get
-          val bytes = ((bytesElem.elementValue match {case ConstValueIndex(index) => constantWrapped(index)})
-                  .asInstanceOf[StringBytesPair].bytes)
-          val length = ByteCodecs.decode(bytes)
-        
-          bytes take length
-      }
-    }
-    
-    def scalaSigFromAnnotation(classFile: ClassFile): Option[ScalaSig] =
-      scalaSigBytesFromAnnotation(classFile) map (xs => ScalaSigAttributeParsers.parse(sp.ByteCode(xs)))
-
-    def scalaSigFromAttribute(classFile: ClassFile) : Option[ScalaSig] =
-      classFile.attribute(SCALA_SIG).map(_.byteCode).map(ScalaSigAttributeParsers.parse)
-    
-    def parse(classFile: ClassFile): Option[ScalaSig] = {
-      val scalaSig  = scalaSigFromAttribute(classFile)
-    
-      scalaSig match {
-        // No entries in ScalaSig attribute implies that the signature is stored in the annotation
-        case Some(ScalaSig(_, _, entries)) if entries.length == 0 =>
-          scalaSigFromAnnotation(classFile)
-        case x => x
-      }
-    }
-    
-    def parse(clazz : Class[_]): Option[ScalaSig] = {
-      val byteCode  = sp.ByteCode.forClass(clazz)
-      val classFile = ClassFileParser.parse(byteCode)
-
-      parse(classFile)
-    }
-  }
 }
