d2bd9c69fbbba59c631536c972b1e2708e3c326d
Paul Phillips <paulp@improving.org>
2011-05-27 11:57:46 -0700

    Integrated sxr.Publish.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,4 +3,5 @@ import sbt._
 class Plugins(info: sbt.ProjectInfo) extends sbt.PluginDefinition(info) {
   val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
   val assemblySBT = "com.codahale" % "assembly-sbt" % "0.1.1"
+  val sxr_publish = "net.databinder" % "sxr-publish" % "0.2.0"
 }
